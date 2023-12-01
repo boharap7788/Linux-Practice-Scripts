933df59b747049c716cd8b6e807435989b306d56
Prakash Bohara <142651511+boharap7788@users.noreply.github.com>
2023-12-02 02:04:53 +0530

    Update usermanagemant.sh

--- usermanagemant.sh
+++ usermanagemant.sh
@@ -8,4 +8,6 @@ useradd -m "$username" -c "$fullname"
 echo "$username:$fullname" | chpasswd
 
 echo "Account created successfully"
-#NOTE : check ( cat /etc/passwd ) / ( cat /etc/shadow )
+#NOTE : After execution of script check ( cat /etc/passwd ) / ( cat /etc/shadow )
+#The -m option ensures that a home directory is created for the user.
+#The -c option is used to add the full name as the user's comment.
